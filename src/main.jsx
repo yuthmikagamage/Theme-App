ac003d896119e2af66125b3690c847c1cb913b0d
yuthmikagamage <vanujyuthmika@gmail.com>
2025-12-30 11:52:26 +0530

    Update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import Task_23 from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(<Task_23 />);
